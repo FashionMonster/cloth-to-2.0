33111fdcbe760cfb2bce475c5c2a348d5b9bd0c9
Yusuke Hirooka <yhirookajpn@gmail.com>
2022-02-24 00:03:54 +0900

    [fixed] deploy error

--- interfaces/ui/components/molecules/contributePage/contributeForm.tsx
+++ interfaces/ui/components/molecules/contributePage/contributeForm.tsx
@@ -1,11 +1,11 @@
+import type { DeepMap, ErrorOption, FieldError } from 'react-hook-form';
+import type { ContributeFormType } from 'constants/types/form/contributeFormType';
 import { SelectCategory } from 'interfaces/ui/components/atoms/selectBoxes/selectCategory';
 import { SelectColor } from 'interfaces/ui/components/atoms/selectBoxes/selectColor';
 import { SelectComposition } from 'interfaces/ui/components/molecules/contributePage/selectComposition';
 import { InputCompositionRatio } from 'interfaces/ui/components/molecules/contributePage/inputCompositionRatio';
-import { InputText } from 'interfaces/ui/components/atoms/textBoxes/inputText';
 import { InputTextarea } from 'interfaces/ui/components/atoms/textareas/inputTextarea';
-import type { DeepMap, ErrorOption, FieldError } from 'react-hook-form';
-import type { ContributeFormType } from 'constants/types/form/contributeFormType';
+import { InputText } from 'interfaces/ui/components/atoms/textBoxes/inputText';
 
 //引数の型定義
 type Props = {
